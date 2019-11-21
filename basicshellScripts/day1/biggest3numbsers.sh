183a019ef920c590fd09812378428eef929a9770
veera-devops <57884392+veera-devops@users.noreply.github.com>
2019-11-21 13:34:15 +0530

    Update biggest3numbsers.sh

--- basicshellScripts/day1/biggest3numbsers.sh
+++ basicshellScripts/day1/biggest3numbsers.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "enter 1st number"
-read fn
-echo "enter 2nd number"
-read sn
-echo "enter 3rd number"
-read tn
+#echo "enter 1st number"
+#read fn
+#echo "enter 2nd number"
+#read sn
+#echo "enter 3rd number"
+#read tn
 
-a=$fn
-b=$sn
-c=$tn
+a=$1
+b=$2
+c=$3
 
 if [ $a -gt $b -a $a -gt $c ]
 then
