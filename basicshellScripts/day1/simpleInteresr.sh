353304deda395cb1095a768c1ad6a7b120c98e42
veera-devops <57884392+veera-devops@users.noreply.github.com>
2019-11-28 15:37:21 +0530

    Update simpleInteresr.sh

--- basicshellScripts/day1/simpleInteresr.sh
+++ basicshellScripts/day1/simpleInteresr.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "enter p value"
-read p
-echo "enter interest rate"
-read r
-echo "enter time"
-read t
+#echo "enter p value"
+#read p
+#echo "enter interest rate"
+#read r
+#echo "enter time"
+#read t
+p=$1
+t=$2
+r=$3
 #u=`expr $p \* $t \* $r`
 si=`expr $p \* $t \* $r / 100`
 echo "Simple Interest is $si"
